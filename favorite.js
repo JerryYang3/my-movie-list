3c70ebba41012b73281e9f5fe7d12619490eac0b
JerryYang <jerry20928@gmail.com>
2020-03-16 08:57:42 +0800

    add Movie List Project

---
+++ favorite.js
@@ -0,0 +1,84 @@
+(function () {
+  const BASE_URL = 'https://movie-list.alphacamp.io'
+  const INDEX_URL = BASE_URL + '/api/v1/movies/'
+  const POSTER_URL = BASE_URL + '/posters/'
+
+  const dataPanel = document.getElementById('data-panel')
+  const data = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+
+  displayDataList(data)
+
+
+  // Listener
+  dataPanel.addEventListener('click', (event) => {
+    const id = event.target.dataset.id
+    if (event.target.matches('.btn-show-movie')) {
+      showMovie(id)
+    }
+    else if (event.target.matches('.btn-remove-favorite')) {
+      removeFavoriteItem(id)
+    }
+  })
+
+
+
+
+
+  function displayDataList(data) {
+    let htmlContent = ''
+    data.forEach(function (item, index) {
+      htmlContent += `
+        <div class="col-sm-3">
+          <div class="card mb-2">
+            <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
+            <div class="card-body movie-item-body">
+              <h5 class="card-title">${item.title}</h5>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
+              <button class="btn btn-danger btn-remove-favorite" data-id="${item.id}">X</button>
+            </div>
+          </div>
+        </div>
+      `
+    })
+    dataPanel.innerHTML = htmlContent
+  }
+
+  function removeFavoriteItem(id) {
+    // find movie by id
+    const index = data.findIndex(item => item.id === Number(id))
+    if (index === -1) return
+
+    // removie movie and update localStorage
+    data.splice(index, 1)
+    localStorage.setItem('favoriteMovies', JSON.stringify(data))
+
+    // repaint dataList
+    displayDataList(data)
+  }
+
+  function showMovie(id) {
+    // get elements
+    const modalTitle = document.getElementById('show-movie-title')
+    const modalImage = document.getElementById('show-movie-image')
+    const modalDate = document.getElementById('show-movie-date')
+    const modalDescription = document.getElementById('show-movie-description')
+
+    // set request url
+    const url = INDEX_URL + id
+    console.log(url)
+    // send request to show api
+    axios.get(url)
+      .then((response) => {
+        // insert data into modal ui
+        const data = response.data.results
+        modalTitle.textContent = data.title
+        modalImage.innerHTML = `<img src="${POSTER_URL}${data.image}" class="img-fluid" alt="Responsive image">`
+        modalDate.textContent = `release at : ${data.release_date}`
+        modalDescription.textContent = `${data.description}`
+      })
+      .catch((err) => console.log(err))
+
+  }
+})()
